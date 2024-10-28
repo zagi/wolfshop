75aad9341c6089d1452c93d2e4cf0a35132f23ab
Michal Zagalski <biuro@zagalski.pl>
2024-10-28 21:27:20 +0100

    Modify Item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,6 +7,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Item extends Model
 {
-   use HasFactory;
-   protected $fillable = ['name', 'quality', 'sellIn'];
+    use HasFactory;
+    protected $fillable = ['name', 'quality', 'sellIn', 'imgUrl'];
+
+    public function __toString(): string
+    {
+        return "{$this->name}, {$this->sellIn}, {$this->quality}";
+    }
+
+    public function getImgUrl(): ?string
+    {
+        return $this->imgUrl;
+    }
+
+    public function setImgUrl(string $imgUrl): self
+    {
+        $this->imgUrl = $imgUrl;
+        return $this;
+    }
 }
