4770eac55a831b833eb3e7340ddabda404b79f4f
Michal Zagalski <biuro@zagalski.pl>
2024-10-28 18:58:50 +0100

    Use HasFactory

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Item extends Model
 {
-    protected $fillable = ['name', 'quality', 'sellIn'];
+   use HasFactory;
+   protected $fillable = ['name', 'quality', 'sellIn'];
 }
