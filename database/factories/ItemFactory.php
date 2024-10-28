e7c32b054c3afae18521fd1e5627e7c0841f9a9d
Michal Zagalski <biuro@zagalski.pl>
2024-10-28 19:20:42 +0100

    Create Item factory

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Item;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ItemFactory extends Factory
+{
+    protected $model = Item::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->randomElement([
+                'Apple AirPods', 'Apple iPad Air', 'Samsung Galaxy S23', 'Xiaomi Redmi Note 13', 'Normal Item'
+            ]),
+            'quality' => $this->faker->numberBetween(0, 50),
+            'sellIn' => $this->faker->numberBetween(-10, 20),
+        ];
+    }
+}
