3cdb3ff4a701041f096abfeecd98e462aa16ad87
Michal Zagalski <biuro@zagalski.pl>
2024-10-30 16:51:09 +0100

    Refactor faker

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -11,10 +11,20 @@ class ItemFactory extends Factory
 
     public function definition()
     {
+        $names = [
+            'Apple AirPods',
+            'Apple iPad Air',
+            'Samsung Galaxy S23',
+            'Xiaomi Redmi Note 13',
+            'Normal Item',
+            'Custom Item',
+            'Another Item'
+        ];
+
+        shuffle($names);
+
         return [
-            'name' => $this->faker->randomElement([
-                'Apple AirPods', 'Apple iPad Air', 'Samsung Galaxy S23', 'Xiaomi Redmi Note 13', 'Normal Item', 'Custom Item', 'Another Item'
-            ]),
+            'name' => $this->faker->unique()->randomElement($names),
             'quality' => $this->faker->numberBetween(0, 50),
             'sellIn' => $this->faker->numberBetween(-10, 20),
         ];
